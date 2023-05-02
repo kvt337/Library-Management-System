8145395f1ad13468c3c2e6d4dfa0f0699f368626
Javier Cardoso <Cardoso.Javier@outlook.com>
2023-05-02 04:44:19 -0500

    Some progress

--- main/CheckoutPage.py
+++ main/CheckoutPage.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import sqlite3
+import datetime
 
 class CheckoutPage(tk.Frame):
 
@@ -32,6 +33,12 @@ class CheckoutPage(tk.Frame):
         branch_label.grid(row=3, column=0)
         self.branch.grid(row=3, column=1)
 
+        # Add the Card_No input field
+        card_no_label = tk.Label(input_frame, text="Card No")
+        self.card_no = tk.Entry(input_frame, width=30)
+        card_no_label.grid(row=4, column=0)
+        self.card_no.grid(row=4, column=1)
+
         # Output of the query
         result_frame = tk.Frame(self)
         self.result_label = tk.Label(result_frame, text="")
@@ -57,48 +64,56 @@ class CheckoutPage(tk.Frame):
     def checkout_book(self):
         book_title_input = self.book_title.get()
         author_input = self.author.get()
-        branch_input = self.branch.get()
+        branch_input = int(self.branch.get())
+        card_no_input = int(self.card_no.get())
 
+        """print(book_title_input)
+        print(author_input)
+        print(branch_input)
+        print(card_no_input)"""
+
+        # connect to the database
         conn = sqlite3.connect('LMS.db')
         cursor = conn.cursor()
 
-        # Checkout book and update Book_Copies table
-        cursor.execute('''
-            UPDATE BOOK_COPIES
-            SET No_Of_Copies = No_Of_Copies - 1
-            WHERE Book_Id IN (
-                SELECT BOOK.Book_Id
-                FROM BOOK
-                JOIN BOOK_AUTHORS ON BOOK.Book_Id = BOOK_AUTHORS.Book_Id
-                WHERE Title = ? AND Author_Name = ?)
-            AND Branch_Id IN (
-                SELECT Branch_Id
-                FROM LIBRARY_BRANCH
-                WHERE Branch_Name = ?)
-        ''', (book_title_input, author_input, branch_input))
-        conn.commit()
+        # Find the book_id based on the title and author
+        cursor.execute('SELECT Book_Id FROM BOOK WHERE Title = ? AND Book_Id IN (SELECT Book_Id FROM BOOK_AUTHORS WHERE Author_Name = ?)', (book_title_input, author_input))
+        book_id = cursor.fetchone()
 
-        # Get the updated number of copies
-        cursor.execute('''
-            SELECT No_Of_Copies
-            FROM BOOK_COPIES
-            WHERE Book_Id IN (
-                SELECT BOOK.Book_Id
-                FROM BOOK
-                JOIN BOOK_AUTHORS ON BOOK.Book_Id = BOOK_AUTHORS.Book_Id
-                WHERE Title = ? AND Author_Name = ?)
-            AND Branch_Id IN (
-                SELECT Branch_Id
-                FROM LIBRARY_BRANCH
-                WHERE Branch_Name = ?)
-        ''', (book_title_input, author_input, branch_input))
+        if book_id is None:
+            self.result_label.config(text="Book not found")
+            return
+
+        # Find the number of copies at the branch
+        cursor.execute('SELECT No_Of_Copies FROM BOOK_COPIES WHERE Book_Id = ? AND Branch_Id = ?', (int(book_id[0]), int(branch_input),))
+        copies = cursor.fetchone()
+
+        if copies is None or copies[0] < 1:
+            self.result_label.config(text="No copies available at this branch")
+            return
         
-        result = cursor.fetchone()
-        if result is not None:
-            updated_copies = result[0]
-            self.result_label.config(text=f"Updated number of copies: {updated_copies}")
-        else:
-            self.result_label.config(text="No matching records found.")
+        # Add a new loan to BOOK_LOANS
+        date_out = datetime.date.today()
+        due_date = date_out + datetime.timedelta(days=14)
 
-        # Close the connection
+        print(book_id[0])
+        print(branch_input)
+        print(card_no_input) 
+        print(date_out)
+        print(due_date)
+
+        cursor.execute('INSERT INTO BOOK_LOANS (Book_Id, Branch_Id, Card_No, Date_Out, Due_Date) VALUES (?, ?, ?, ?, ?)', (int(book_id[0]), int(branch_input), int(card_no_input), date_out, due_date))
+
+        # Update the number of copies in BOOK_COPIES
+        updated_copies = copies[0] - 1
+        cursor.execute('UPDATE BOOK_COPIES SET No_Of_Copies = ? WHERE Book_Id = ? AND Branch_Id = ?)', (int(updated_copies), int(book_id[0]), int(branch_input)))
+
+        # commit changes and close the connection
+        conn.commit()
         conn.close()
+
+        self.result_label.config(text="Book checked out successfully. Updated number of copies: {}".format(updated_copies))
+        self.book_title.delete(0, 'end')
+        self.author.delete(0, 'end')
+        self.branch.delete(0, 'end')
+        self.card_no.delete(0, 'end')
